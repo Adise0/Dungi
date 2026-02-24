0596f2ac8c10a57c581ced0433a5f2f1da0cef10
Adise <admin@pauibanez.me>
2026-02-24 09:58:21 +0100

    added the new dungeon screen

---
+++ assets/ui/MainMenu.js
@@ -0,0 +1,15 @@
+const Quit = () => {
+  Send("Quit");
+};
+
+const Back = () => {
+  window.location = "../MainMenu/MainMenu.html";
+};
+
+const NewDungeon = () => {
+  window.location = "../NewDungeon/NewDungeon.html";
+};
+
+const LoadDungeon = () => {
+  window.location = "../LoadDungeon/LoadDungeon.html";
+};
